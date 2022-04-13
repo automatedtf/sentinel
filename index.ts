5c3c7edf855a753ef55dd536e5d82b2387d0d349
Mengh <hilliamtung@gmail.com>
2022-04-13 11:18:23 +0100

    :goal_net: Top-level error catch

--- index.ts
+++ index.ts
@@ -3,19 +3,25 @@ import axios from 'axios';
 import { serialiseData, SteamEvents, SteamReactor } from '@automatedtf/reactor';
 
 // Set up bot
-const bot = new SteamReactor({
-    steamid: process.env.STEAMID,
-    accountName: process.env.ACCOUNT_NAME,
-    password: process.env.ACCOUNT_PASSWORD,
-    sharedSecret: process.env.SHARED_SECRET,
-    logonID: process.env.LOGON_ID ? parseInt(process.env.LOGON_ID) : undefined
-});
-
-// Send all events upstream if an upstream is set
-if (process.env.UPSTREAM != null) {
-    Object.values(SteamEvents).forEach((event) => {
-        bot.on(event, (data) => {
-            axios.post(process.env.UPSTREAM, { steamid: process.env.STEAMID, event, data: serialiseData(data) });
-        });
+try {
+    const bot = new SteamReactor({
+        steamid: process.env.STEAMID,
+        accountName: process.env.ACCOUNT_NAME,
+        password: process.env.ACCOUNT_PASSWORD,
+        sharedSecret: process.env.SHARED_SECRET,
+        logonID: process.env.LOGON_ID ? parseInt(process.env.LOGON_ID) : undefined
     });
+
+    // Send all events upstream if an upstream is set
+    if (process.env.UPSTREAM != null) {
+        Object.values(SteamEvents).forEach((event) => {
+            bot.on(event, (data) => {
+                axios.post(process.env.UPSTREAM, { steamid: process.env.STEAMID, event, data: serialiseData(data) });
+            });
+        });
+    }
+} catch (error) {
+    if (process.env.UPSTREAM != null) {
+        axios.post(process.env.UPSTREAM, { steamid: process.env.STEAMID, event: "SentinelError", data: error.message });
+    }
 }
